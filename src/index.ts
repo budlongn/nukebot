76221a8f18861e30404d8ca13cf513fbe2cfe76f
Tyler <tylereisenhauer7372@gmail.com>
2020-02-10 21:35:05 -0600

    Add the bot

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,47 @@
-import {Client, GuildChannel, GuildMember} from 'discord.js'
+import { Client, Collection, GuildMember, Message, VoiceChannel } from 'discord.js'
 
 const client = new Client()
+const prefix = '!'
 
-client.on('message', message => {
-    if (!message.guild) return;
+client.on('ready', () => {
+    console.log('Bot Online')
+    client.user.setPresence({
+        status: 'online',
+        game: {
+            name: 'Kicking Doors and Slapping Whores',
+            type: 'PLAYING'
+        }
+    })
+})
 
-    if (message.content.startsWith('!move')) {
-        const channels = message.guild.channels.filter((x: GuildChannel) => {
-            return x.type === 'voice'
-        })
+client.on('message', async (message: Message) => {
+    if (message.author.bot) return
+    if (!message.guild) return
+    if (!message.content.startsWith(prefix)) return
 
-        if (channels.array().length != 2) {
-            message.reply('Mention exactly TWO channels retard.')
+    const args: string[] = message.content.trim().match(/[^\s"']+|"([^"]*)"|'([^']*)'/g).map((arg) => {
+        return arg.replace(/['"]/g, '')
+    })
+    const command: string = args.shift()
+
+    if (command === '!move') {
+        if (args.length != 2) {
+            return await message.reply('Mention exactly TWO channels retard.')
         }
 
-        const users: Array<GuildMember> = channels[0].members.array()
+        const fromChannel = message.guild.channels.find((x) => {
+            return x.type === 'voice' && x.name === args[0]
+        }) as VoiceChannel
+        const toChannel = message.guild.channels.find((x) => {
+            return x.type === 'voice' && x.name === args[1]
+        }) as VoiceChannel
+
+        const users: Collection<string, GuildMember> = fromChannel.members
 
         users.forEach((user: GuildMember) => {
-
+            user.setVoiceChannel(toChannel.id)
         })
     }
 })
+
+client.login('<DISCORD_BOT_KEY_GOES_HERE>')
