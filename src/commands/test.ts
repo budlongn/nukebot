e93172e1ec10572da83e864125c14908c0cf615e
Tyler Eisenhauer <tyler@teamsoftware.com>
2020-02-12 12:55:48 -0600

    remove test endpoint

--- src/commands/test.ts
+++
@@ -1,5 +0,0 @@
-import {Message} from 'discord.js'
-
-export default async (args: string[], message: Message) => {
-    return await message.channel.send('Received the test!')
-}
--- src/handlers/command.ts
+++ src/handlers/command.ts
@@ -1,13 +1,10 @@
 import {Message} from 'discord.js'
 import move from '../commands/move'
-import test from '../commands/test'
 
 export default async (command: string, args: string[], message: Message) => {
     switch (command) {
         case 'move':
             return await move(args, message)
-        case 'test':
-            return await test(args, message)
         default:
             return await message.channel.send('I have no idea what the FUCK you\'re talking about')
     }
