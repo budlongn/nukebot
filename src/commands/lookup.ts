800740060d1227f71e93c0b1410689f1e2ee1187
Tyler <tylereisenhauer7372@gmail.com>
2020-02-21 18:19:58 -0600

    formatting

--- src/commands/lookup.ts
+++ src/commands/lookup.ts
@@ -2,11 +2,19 @@ import {Message} from 'discord.js'
 import {getBlizzardClient} from "../config/config"
 import {Boss, Character, Class, Raid, Spec, Talents} from "../types/battlenet"
 import {find, get} from 'lodash'
+
 export async function lookup(args: string[], message: Message) {
     const client = await getBlizzardClient()
-    const resp = await client.wow.character(['progression', 'talents', 'items'], {origin: 'us', realm: args[0], name: args[1]})
+    const resp = await client.wow.character(['progression', 'talents', 'items'], {
+        origin: 'us',
+        realm: args[0],
+        name: args[1]
+    })
     const character: Character = resp.data
-    const spec: Spec = find(character.talents, (x: Talents) => {return x.selected}).spec
+    const spec: Spec = find(character.talents, (x: Talents) => {
+        return x.selected
+    }).spec
+
     await message.channel.send({
         embed: {
             author: {
@@ -42,7 +50,7 @@ function buildRecentRaidList(raids: Raid[]): string {
         const count: number = raid.bosses.filter((x: Boss) => {
             return get(x, `${highestDifficulty}Kills`, 0) > 0
         }).length
-        progString = progString.concat(`**${raid.name}:** ${count}/${raid.bosses.length}${highestDifficulty.slice(0,1).toUpperCase()}\n`)
+        progString = progString.concat(`**${raid.name}:** ${count}/${raid.bosses.length}${highestDifficulty.slice(0, 1).toUpperCase()}\n`)
     }
     return progString
 }
