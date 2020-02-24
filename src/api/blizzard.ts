308de6bfb55d6a3476156c84232fcfb1f444417d
Tyler <tylereisenhauer7372@gmail.com>
2020-02-23 18:32:51 -0600

    Refresh token on 401 from blizzard (#3)

--- src/api/blizzard.ts
+++ src/api/blizzard.ts
@@ -3,22 +3,41 @@ import {Character} from "../types/character.Types"
 
 let apiClient: AxiosInstance
 
-export async function initializeBlizzardClient() {
-    if (!apiClient) {
-        const {data: authRequest} = await axios.get('https://us.battle.net/oauth/token', {
-            params: {
-                grant_type: 'client_credentials',
-                client_id: process.env.BATTLE_NET_API_CLIENT_ID,
-                client_secret: process.env.BATTLE_NET_API_SECRET
-            }
-        })
+async function getAuthToken(): Promise<string> {
+    const {data: authRequest} = await axios.get('https://us.battle.net/oauth/token', {
+        params: {
+            grant_type: 'client_credentials',
+            client_id: process.env.BATTLE_NET_API_CLIENT_ID,
+            client_secret: process.env.BATTLE_NET_API_SECRET
+        }
+    })
 
+    return `Bearer ${authRequest.access_token}`
+}
+
+export async function initializeBlizzardClient(): Promise<void> {
+    if (!apiClient) {
         apiClient = axios.create({
             baseURL: 'https://us.api.blizzard.com',
             headers: {
-                Authorization: `Bearer ${authRequest.access_token}`
+                Authorization: await getAuthToken()
             },
-            timeout: 5000
+            timeout: 5000,
+        })
+
+        apiClient.interceptors.response.use(null, async (error) => {
+            if (error.config && error.response && error.response.status === 401) {
+                const token = await getAuthToken()
+                apiClient.defaults.headers = {
+                    Authorization: token
+                }
+                error.config.headers = {
+                    Authorization: token
+                }
+                return apiClient.request(error.config)
+            }
+
+            return Promise.reject(error)
         })
     }
 }
--- src/api/raiderio.ts
+++ src/api/raiderio.ts
@@ -3,14 +3,14 @@ import {RaiderIOCharacterData} from "../types/raiderio.Types"
 
 let apiClient: AxiosInstance
 
-export function initializeRaiderIOClient() {
+export function initializeRaiderIOClient(): void {
     apiClient = axios.create({
         baseURL: 'https://raider.io',
         timeout: 5000 //raider io sucks and is down/slow A LOT
     })
 }
 
-export async function getCharacterData(fields: string[], realm: string, name: string) {
+export async function getCharacterData(fields: string[], realm: string, name: string): Promise<RaiderIOCharacterData> {
     try {
         const {data} = await apiClient.get<RaiderIOCharacterData>('/api/v1/characters/profile', {
             params: {
