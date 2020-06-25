a7d54ca8978392eef8e1f268c986069e6ed9078c
Tyler <tylereisenhauer7372@gmail.com>
2020-06-25 15:02:55 -0500

    undo trolling gama harder

--- src/commands/gamapost.ts
+++ src/commands/gamapost.ts
@@ -3,8 +3,6 @@ import {Message} from 'discord.js'
 export async function gamapost(message: Message) {
     const regex: RegExp = /(https?|chrome):\/\/[^\s$.?#].[^\s]*/gm
     if (regex.test(message.content)) {
-        await message.delete()
-
-        return message.channel.send(`WARNING, POTENTIAL GAMAPOST, PROCEED WITH CAUTION\n\n||${message.content}||`)
+        return message.channel.send('WARNING, POTENTIAL GAMAPOST, PROCEED WITH CAUTION')
     }
 }
