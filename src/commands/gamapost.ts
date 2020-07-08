e63a250ca346a8705f767cf79dd997e230623dea
Tyler <tylereisenhauer7372@gmail.com>
2020-07-08 09:05:20 -0500

    remove console log

--- src/commands/gamapost.ts
+++ src/commands/gamapost.ts
@@ -2,7 +2,7 @@ import {Message} from 'discord.js'
 
 export async function gamapost(message: Message) {
     const regex: RegExp = /(https?|chrome):\/\/[^\s$.?#].[^\s]*/gm
-    console.log(message.attachments.first())
+
     if (regex.test(message.content) || message.attachments.first()) {
         return message.channel.send('WARNING, POTENTIAL GAMAPOST, PROCEED WITH CAUTION')
     }
