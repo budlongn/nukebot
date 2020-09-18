5f6cd8a5dc7436665263d04bb53f2d45e2ea6d71
Tyler Eisenhauer <tylereisenhauer7372@gmail.com>
2020-09-18 18:24:35 -0500

    update polling time

--- src/page-watchers/nvidia.ts
+++ src/page-watchers/nvidia.ts
@@ -14,7 +14,7 @@ export const nvidiaCheck = async (webhookClient: WebhookClient) => {
             width: 2560,
             height: 1440
         })
-        await page.goto(url)
+        await page.goto(url, {waitUntil: "networkidle0"})
         await sleep(5000)
         const newProduct: string = await page.evaluate(() => {
             return document.querySelector('#mainCont > featured-product > div > div')?.outerHTML
