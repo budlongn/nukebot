318ff371276314d3dec45f4e88d696fb0da32eb6
Tyler <tylereisenhauer7372@gmail.com>
2021-08-02 10:18:38 -0500

    Add Automated Docker Build (#27)
    * Docker Build
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * Update azure-pipelines.yml for Azure Pipelines
    
    * test

--- src/commands/assume.ts
+++ src/commands/assume.ts
@@ -1,5 +1,5 @@
 import {Message} from 'discord.js'
 
 export async function assume(message: Message) {
-    return await message.channel.send('You assumin little ass bitch -Taner 2021')
+    return await message.channel.send('\"You assumin little ass bitch\" -Taner 2021')
 }
